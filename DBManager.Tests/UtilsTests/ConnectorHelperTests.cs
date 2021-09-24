7f47bbbdaf89733c6a13483eea67a0e6a84b9796
Krzysztof Kruglak <kru_krz@o2.pl>
2021-09-24 19:39:15 +0200

    DBM-17 Init xuint tests solution

---
+++ DBManager.Tests/UtilsTests/ConnectorHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using DBManager.Utils;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DBManager.Tests.UtilsTests
+{
+    public class ConnectorHelperTests
+    {
+        [Fact]
+        public void ForParametersInDictionary_ReturnsCorrectConnectionString()
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                { "Url", "localhost" },
+                { "Port", "3306" },
+                { "Uid", "user" },
+                { "Password", "pwd123" }
+            };
+
+            var results = ConnectorHelper.Combine(parameters);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+            Assert.Contains("Url=localhost;", results);
+            Assert.Contains("Port=3306;", results);
+            Assert.Contains("Uid=user;", results);
+            Assert.Contains("Password=pwd123;", results);
+        }
+    }
+}
