2fbc1fb16de4326100e134abd9a1ed1e4a3da388
Krzysztof Kruglak <kru_krz@o2.pl>
2021-09-28 18:05:00 +0200

    DBM-22 Fix remove connection error

--- DBManager/Presenters/DBManagerPresenterBase.cs
+++ DBManager/Presenters/DBManagerPresenterBase.cs
@@ -3,9 +3,11 @@ using System.Threading.Tasks;
 
 namespace DBManager.Presenters
 {
-    public abstract class DBManagerPresenterBase : DBManagerPresenterDatabaseOperationBase
+    public abstract class DBManagerPresenterBase : PresenterBase
     {
         public abstract Task<Response> AddConnection(AddConnectionDto dto);
         public abstract Task<Response> RemoveConnection(string connectionName);
+        public abstract Response GetConnectionNames();
+        public abstract Response GetPresenter(string connectionName);
     }
 }
--- DBManager/Presenters/DBManagerPresenterDatabaseOperationBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DBManager.Presenters
-{
-    public abstract class DBManagerPresenterDatabaseOperationBase : PresenterBase
-    {
-        public abstract Response GetConnectionNames();
-        public abstract Response GetPresenter(string connectionName);
-    }
-}
