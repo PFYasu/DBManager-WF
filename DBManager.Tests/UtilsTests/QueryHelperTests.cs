b16fd8de50ef46a26ecb28fc8ea7644b1cd29db9
Krzysztof Kruglak <kru_krz@o2.pl>
2021-10-03 00:21:41 +0200

    DBM-4 Add QueryHelper and unit tests

---
+++ DBManager.Tests/UtilsTests/QueryHelperTests.cs
@@ -0,0 +1,60 @@
+﻿using DBManager.Utils;
+using Xunit;
+
+namespace DBManager.Tests.UtilsTests
+{
+    public class QueryHelperTests
+    {
+        [Theory]
+        [InlineData("SELECT * FROM database;")]
+        [InlineData("select * from database;")]
+        [InlineData("SELECT * from database;")]
+        [InlineData("select * FROM database;")]
+        [InlineData("SELECT firstName, lastName FROM employees where Age < 30;")]
+        public void ForQuery_WithSelectStatement_ReturnsQueryType(string query)
+        {
+            var result = Act(query);
+
+            Assert.Equal(QueryType.Query, result);
+        }
+        
+        [Theory]
+        [InlineData("UPDATE employees SET salary = 3000 WHERE salary < 2500;")]       
+        [InlineData("UPDATE employees SET selection = 2 WHERE selection = 1;")]
+        [InlineData("Update selectTable set fromColumn = 'test' WHERE fromColumn = 'otherTest';")]
+        public void ForNonQuery_WithUpdateStatement_ReturnsNonQueryType(string query)
+        {
+            var result = Act(query);
+
+            Assert.Equal(QueryType.NonQuery, result);
+        }
+
+        [Theory]
+        [InlineData("INSERT INTO test (`ID`, `Age`) VALUES ('id1', 23);")]
+        [InlineData("INSERT INTO selectTable VALUES (1, 2, 3);")]
+        public void ForNonQuery_WithInsertStatement_ReturnsNonQueryType(string query)
+        {
+            var result = Act(query);
+
+            Assert.Equal(QueryType.NonQuery, result);
+        }
+
+        [Theory]
+        [InlineData("DELETE FROM selectTable WHERE fromColumn < 5;")]
+        [InlineData("DELETE FROM fromTable WHERE selectColumn = 5 AND select_FromDb = 3;")]
+        public void ForNonQuery_WithDeleteStatement_ReturnsNonQueryType(string query)
+        {
+            var result = Act(query);
+
+            Assert.Equal(QueryType.NonQuery, result);
+        }
+        
+
+        public QueryType Act(string query)
+        {
+            var result = QueryHelper.GetQueryType(query);
+
+            return result;
+        }
+    }
+}
---
+++ DBManager/Utils/QueryHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DBManager.Utils
+{
+    public static class QueryHelper
+    {
+        private static readonly Regex _regex;
+
+        static QueryHelper()
+        {
+            _regex = new Regex("^(?i)select (.*?)(?i)from ");
+        }
+
+        public static QueryType GetQueryType(string query)
+        {
+            var match = _regex.Match(query);
+
+            if (match.Success)
+                return QueryType.Query;
+
+            return QueryType.NonQuery;
+        }
+    }
+
+    public enum QueryType
+    {
+        Query,
+        NonQuery
+    }
+}
