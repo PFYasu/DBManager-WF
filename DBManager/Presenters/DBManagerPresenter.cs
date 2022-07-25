4699de5a8c518b8cf43202a3b6e230f2454ed24c
Krzysztof Kruglak <kru_krz@o2.pl>
2022-07-25 21:58:45 +0200

    DBM-75 Extract DBManagerService

--- DBManager/Presenters/DBManagerPresenter.cs
+++ DBManager/Presenters/DBManagerPresenter.cs
@@ -1,276 +1,103 @@
-﻿using DBManager.Core;
-using DBManager.Core.Dto;
-using DBManager.Core.Models;
-using DBManager.Core.Models.Engines;
+﻿using DBManager.Core.Dto;
 using DBManager.Core.Presenters;
 using DBManager.Core.Presenters.Engines;
 using DBManager.Models;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DBManager.Presenters
 {
     public class DBManagerPresenter : IDBManagerPresenter
     {
-        private readonly IDBManagerModel _model;
-        private readonly Dictionary<string, IEnginePresenter> _presenters;
-        private readonly DataTransferMethods _dataTransferMethods;
+        private readonly DBManagerService _service;
 
         public DBManagerPresenter(IDBManagerModel model)
         {
-            _model = model;
-            _presenters = new Dictionary<string, IEnginePresenter>();
-
-            _dataTransferMethods = new DataTransferMethods
+            var dataTransferMethods = new DataTransferMethods
             {
                 GetConnectionNames = GetConnectionNames,
                 GetPresenter = GetPresenter
             };
 
-            InitializeEnginePresenters();
+            _service = new DBManagerService(model, dataTransferMethods);
+            _service.InitializeEnginePresenters();
         }
-
-        private void InitializeEnginePresenters()
-        {
-            List<string> connectionNames;
-
-            try
-            {
-                connectionNames = _model.GetConnectionNames();
-            }
-            catch (Exception exception)
-            {
-                throw new InvalidOperationException(
-                    $"Unable to get connection names for initiate connections.\n{exception.Message}");
-            }
-
-            foreach (var connectionName in connectionNames)
-            {
-                Connection connection;
-
-                try
-                {
-                    connection = _model.GetConnection(connectionName);
-                }
-                catch (Exception exception)
-                {
-                    throw new InvalidOperationException(
-                        $"Unable to initialize {connectionName} connection.\n{exception.Message}");
-                }
-
-                var engineType = connection.EngineType;
-
-                if (EngineModules.Attributes.TryGetValue(engineType, out var engineModuleAttribute) == false)
-                    throw new NotImplementedException(
-                        $"{engineType} engine type is not supported.");
-
-                var presenter = CreatePresenter(engineModuleAttribute, connection);
-
-                if (_presenters.ContainsKey(connectionName))
-                    throw new NotImplementedException(
-                        $"{connectionName} connection already exists in dictionary.");
-                else
-                    _presenters.Add(connectionName, presenter);
-            }
-        }
-
         public async Task<Response> AddConnection(AddConnectionDto dto)
         {
-            if (ConnectionExists(dto.Name))
-                return Response.Error($"Connection with {dto.Name} name already exists");
-
-            var connection = Connection.FromDto(dto);
-
             try
             {
-                _model.AddConnection(connection);
-                await _model.SaveConnections();
-                await _model.LoadConnections();
+                await _service.AddConnection(dto);
+                return Response.Ok();
             }
             catch (Exception exception)
             {
                 return Response.Error(exception.Message);
             }
-
-            return Response.Ok();
         }
 
         public Response GetPresenter(string connectionName)
         {
-            PresenterResponseDto dto;
-
-            if (_presenters.TryGetValue(connectionName, out IEnginePresenter presenterFromDictionary))
-            {
-                dto = new PresenterResponseDto
-                {
-                    EngineType = presenterFromDictionary.EngineType,
-                    Presenter = presenterFromDictionary
-                };
-
-                return Response.Ok(dto);
-            }
-
-            if (ConnectionExists(connectionName) == false)
-                return Response.Error($"Connection with {connectionName} name does not exist");
-
-            Connection connection;
-
             try
             {
-                connection = _model.GetConnection(connectionName);
+                var dto = _service.GetPresenter(connectionName);
+                return Response.Ok(dto);
             }
             catch (Exception exception)
             {
                 return Response.Error(exception.Message);
             }
-
-            var engineType = connection.EngineType;
-
-            if (EngineModules.Attributes.TryGetValue(engineType, out var engineModuleAttribute) == false)
-                return Response.Error("Unable to create presenter. Incorrect engine type.");
-
-            var presenter = CreatePresenter(engineModuleAttribute, connection);
-
-            dto = new PresenterResponseDto
-            {
-                EngineType = engineType,
-                Presenter = presenter
-            };
-
-            if (_presenters.ContainsKey(connectionName))
-                return Response.Error($"Connection {connectionName} already exists in dictionary.");
-            else
-                _presenters.Add(connectionName, presenter);
-
-            return Response.Ok(dto);
         }
 
         public async Task<Response> RemoveConnection(string connectionName)
         {
-            if (ConnectionExists(connectionName) == false)
-                return Response.Error($"Connection with {connectionName} name does not exist");
-
             try
             {
-                _model.RemoveConnection(connectionName);
-                await _model.SaveConnections();
-                await _model.LoadConnections();
+                await _service.RemoveConnection(connectionName);
+                return Response.Ok();
             }
             catch (Exception exception)
             {
                 return Response.Error(exception.Message);
             }
-
-            _presenters.Remove(connectionName);
-
-            return Response.Ok();
         }
 
         public async Task<Response> UpdateConnection(UpdateConnectionDto dto)
         {
-            if (ConnectionExists(dto.OldName) == false)
-                return Response.Error($"{dto.OldName} connection does not exist");
-
-            if (dto.OldName != dto.Name
-                && ConnectionExists(dto.Name))
-                return Response.Error($"Unable to change {dto.OldName} connection - {dto.Name} connection already exists");
-
-            var connection = Connection.FromDto(dto);
-
             try
             {
-                _model.RemoveConnection(dto.OldName);
-                _model.AddConnection(connection);
-                await _model.SaveConnections();
-                await _model.LoadConnections();
+                await _service.UpdateConnection(dto);
+                return Response.Ok();
             }
             catch (Exception exception)
             {
                 return Response.Error(exception.Message);
             }
-
-            _presenters.Remove(dto.OldName);
-
-            return Response.Ok();
         }
 
         public Response GetConnectionNames()
         {
-            List<string> names;
-
             try
             {
-                names = _model.GetConnectionNames();
+                var dto = _service.GetConnectionNames();
+                return Response.Ok(dto);
             }
             catch (Exception exception)
             {
                 return Response.Error(exception.Message);
             }
-
-            var dto = new ConnectionNamesResponseDto { Names = names };
-
-            return Response.Ok(dto);
         }
 
         public Response GetConnectionSettings(string connectionName)
         {
-            if (ConnectionExists(connectionName) == false)
-                return Response.Error($"Connection with {connectionName} does not exist");
-
-            Connection connection;
-
             try
             {
-                connection = _model.GetConnection(connectionName);
+                var dto = _service.GetConnectionSettings(connectionName);
+                return Response.Ok(dto);
             }
             catch (Exception exception)
             {
                 return Response.Error(exception.Message);
             }
-
-            var engineType = connection.EngineType;
-            var connectionParameters = connection.ConnectionParameters;
-            var name = connection.Name;
-
-            var dto = new AddConnectionDto
-            {
-                EngineType = engineType,
-                ConnectionParameters = connectionParameters,
-                Name = name
-            };
-
-            return Response.Ok(dto);
-        }
-
-        private IEnginePresenter CreatePresenter(EngineModuleAttribute engineModuleAttribute, Connection connection)
-        {
-            var model = (IEngineModel)Activator.CreateInstance(
-                engineModuleAttribute.Model,
-                connection);
-
-            var presenter = (IEnginePresenter)Activator.CreateInstance(
-                engineModuleAttribute.Presenter,
-                model,
-                _dataTransferMethods);
-
-            return presenter;
-        }
-
-        private bool ConnectionExists(string connectionName)
-        {
-            List<string> connectionNames;
-
-            try
-            {
-                connectionNames = _model.GetConnectionNames();
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-
-            return connectionNames.Contains(connectionName);
         }
     }
 }
---
+++ DBManager/Presenters/DBManagerService.cs
@@ -0,0 +1,178 @@
+﻿using DBManager.Core;
+using DBManager.Core.Dto;
+using DBManager.Core.Models;
+using DBManager.Core.Models.Engines;
+using DBManager.Core.Presenters.Engines;
+using DBManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DBManager.Presenters
+{
+    public class DBManagerService
+    {
+        private readonly IDBManagerModel _model;
+        private readonly DataTransferMethods _dataTransferMethods;
+        private readonly Dictionary<string, IEnginePresenter> _presenters = new Dictionary<string, IEnginePresenter>();
+
+        public DBManagerService(IDBManagerModel model, DataTransferMethods dataTransferMethods)
+        {
+            _model = model;
+            _dataTransferMethods = dataTransferMethods;
+        }
+
+        public void InitializeEnginePresenters()
+        {
+            var connectionNames = _model.GetConnectionNames();
+
+            foreach (var connectionName in connectionNames)
+            {
+                var connection = _model.GetConnection(connectionName);
+
+                var engineType = connection.EngineType;
+
+                if (EngineModules.Attributes.TryGetValue(engineType, out var engineModuleAttribute) == false)
+                    throw new NotImplementedException($"{engineType} engine type is not supported.");
+
+                var presenter = CreatePresenter(engineModuleAttribute, connection);
+
+                if (_presenters.ContainsKey(connectionName))
+                    throw new NotImplementedException($"{connectionName} connection already exists in dictionary.");
+                    
+                _presenters.Add(connectionName, presenter);
+            }
+        }
+
+        public async Task AddConnection(AddConnectionDto dto)
+        {
+            if (ConnectionExists(dto.Name))
+                throw new InvalidOperationException($"Connection with {dto.Name} name already exists.");
+
+            var connection = Connection.FromDto(dto);
+
+            _model.AddConnection(connection);
+            await _model.SaveConnections();
+            await _model.LoadConnections();
+        }
+
+        public PresenterResponseDto GetPresenter(string connectionName)
+        {
+            if (_presenters.TryGetValue(connectionName, out IEnginePresenter presenterFromDictionary))
+            {
+                return new PresenterResponseDto
+                {
+                    EngineType = presenterFromDictionary.EngineType,
+                    Presenter = presenterFromDictionary
+                };
+            }
+
+            if (ConnectionExists(connectionName) == false)
+                throw new InvalidOperationException($"Connection with {connectionName} name does not exist.");
+
+            var connection = _model.GetConnection(connectionName);
+            var engineType = connection.EngineType;
+
+            if (EngineModules.Attributes.TryGetValue(engineType, out var engineModuleAttribute) == false)
+                throw new InvalidOperationException("Unable to create presenter. Incorrect engine type.");
+
+            var presenter = CreatePresenter(engineModuleAttribute, connection);
+
+            if (_presenters.ContainsKey(connectionName))
+                throw new InvalidOperationException($"Connection {connectionName} already exists in dictionary.");
+                
+            _presenters.Add(connectionName, presenter);
+
+            var dto = new PresenterResponseDto
+            {
+                EngineType = engineType,
+                Presenter = presenter
+            };
+
+            return dto;
+        }
+
+        public async Task RemoveConnection(string connectionName)
+        {
+            if (ConnectionExists(connectionName) == false)
+                throw new InvalidOperationException($"Connection with {connectionName} name does not exist.");
+
+            _model.RemoveConnection(connectionName);
+            await _model.SaveConnections();
+            await _model.LoadConnections();
+
+            _presenters.Remove(connectionName);
+        }
+
+        public async Task UpdateConnection(UpdateConnectionDto dto)
+        {
+            if (ConnectionExists(dto.OldName) == false)
+                throw new InvalidOperationException($"{dto.OldName} connection does not exist.");
+
+            if (dto.OldName != dto.Name && ConnectionExists(dto.Name))
+                throw new InvalidOperationException($"Unable to change {dto.OldName} connection - {dto.Name} connection already exists.");
+
+            var newConnection = Connection.FromDto(dto);
+
+            _model.RemoveConnection(dto.OldName);
+            _model.AddConnection(newConnection);
+            await _model.SaveConnections();
+            await _model.LoadConnections();
+
+            _presenters.Remove(dto.OldName);
+        }
+
+        public ConnectionNamesResponseDto GetConnectionNames()
+        {
+            var names = _model.GetConnectionNames();
+
+            var dto = new ConnectionNamesResponseDto { Names = names };
+
+            return dto;
+        }
+
+        public AddConnectionDto GetConnectionSettings(string connectionName)
+        {
+            if (ConnectionExists(connectionName) == false)
+                throw new InvalidOperationException($"Connection with {connectionName} does not exist");
+
+            var connection = _model.GetConnection(connectionName);
+
+            var dto = new AddConnectionDto
+            {
+                EngineType = connection.EngineType,
+                ConnectionParameters = connection.ConnectionParameters,
+                Name = connection.Name
+            };
+
+            return dto;
+        }
+
+        private bool ConnectionExists(string connectionName)
+        {
+            try
+            {
+                var connectionNames = _model.GetConnectionNames();
+                return connectionNames.Contains(connectionName);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private IEnginePresenter CreatePresenter(EngineModuleAttribute engineModuleAttribute, Connection connection)
+        {
+            var model = (IEngineModel)Activator.CreateInstance(
+                engineModuleAttribute.Model,
+                connection);
+
+            var presenter = (IEnginePresenter)Activator.CreateInstance(
+                engineModuleAttribute.Presenter,
+                model,
+                _dataTransferMethods);
+
+            return presenter;
+        }
+    }
+}
